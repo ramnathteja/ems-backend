f90488e0de24295ea16743a446941f4cd4ce9d82
Ramnath.T.Chekka <ch.ramnathteja@gmail.com>
2024-09-13 21:25:22 +0530

    first commit

---
+++ src/main/java/com/example/ems/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.example.ems.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ems.dto.EmployeeDto;
+import com.example.ems.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // build add employee rest api
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    // build get employee rest api
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+}
---
+++ src/main/java/com/example/ems/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.ems.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.ems.dto.EmployeeDto;
+import com.example.ems.entity.Employee;
+import com.example.ems.exception.ResourceNotFoundException;
+import com.example.ems.mapper.EmployeeMapper;
+import com.example.ems.repository.EmployeeRepository;
+import com.example.ems.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Employee does not exist with the given id: " + employeeId));
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+}
